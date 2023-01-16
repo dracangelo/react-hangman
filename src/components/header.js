f3c94836cdbf7887af8dbef72a452e97e57ba4ae
dracangelo <kevinmbuguaw@gmail.com>
2023-01-16 11:57:40 +0300

    created a figure.js component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <>
+      <h1>Hangman</h1>
+      <p>find the hidden word or die</p>
+      </>
+    </div>
+  )
+}
+
+export default Header
