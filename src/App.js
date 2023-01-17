2063e180e0f94e0b3258fcec250ce81f237a9ed7
dracangelo <kevinmbuguaw@gmail.com>
2023-01-17 10:58:48 +0300

    modified wrongletters and word components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './components/Header.js';
 import Figure from './components/Figure.js';
 import WrongLetters from './components/WrongLetters.js';
 import Word from './components/Word.js';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setwrongLetters] = useState([]);
   return (
     <>
     <Header/>
     <div className="game-container">
       <Figure/>
       <WrongLetters/>
-      <Word/>
+      <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
     </div>
     </>
   );
